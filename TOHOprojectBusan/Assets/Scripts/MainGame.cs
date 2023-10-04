bca615ab5dbe1763d5997504ba6d7ce61e9cd681
rasillrus <rasillrus@naver.com>
2023-10-04 15:35:56 +0900

    no message

--- TOHOprojectBusan/Assets/Scripts/MainGame.cs
+++ TOHOprojectBusan/Assets/Scripts/MainGame.cs
@@ -18,6 +18,10 @@ public class MainGame : MonoBehaviour
         //몬스터 자동 생성 기능 작동
     }
 
+    /// <summary>
+    /// 게임이 시작할 때 부르는 함수, 업데이트 기능을 깨움
+    /// </summary>
+    /// <param name="lbl"></param>
     public void GamePlay()
     {
         //스탯 초기화, 게임 플레이어 생성
@@ -25,14 +29,22 @@ public class MainGame : MonoBehaviour
         //몬스터 생성 패턴 스위치 활성화
     }
 
+    /// <summary>
+    /// 게임 진행을 멈추고 게임 오버 UI 화면을 켜는 함수    
+    /// </summary>
+    /// <param name="lbl"></param>
     public void GameOver()
     {
-        //플레이어가 죽은 경우 게임 진행을 멈추고 게임 오버 UI 화면을 켜기
+ 
     }
 
+    /// <summary>
+    /// 게임 진행을 멈추고 클리어 UI 화면 켜는 함수 
+    /// </summary>
+    /// <param name="lbl"></param>
     public void GameClear()
     {
-        //플레이어가 게임을 성공한 경우 게임 진행을 멈추고 클리어 UI 화면 켜기
+        
     }
 
 
