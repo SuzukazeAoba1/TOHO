6c141323b97e536b008d2718c4c25b0ac6082756
JaeHO <noskang6699.naver.com>
2023-10-04 16:07:32 +0900

    코드정리함

--- TOHOprojectBusan/Assets/Scripts/Ingame/Player.cs
+++ TOHOprojectBusan/Assets/Scripts/Ingame/Player.cs
@@ -5,17 +5,27 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public int HP;
-    public double speed;
-
+    public float speed = 4.5f;
+    private Vector3 position = Vector3.zero;
+    private float H;
+    private float V;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        H = Input.GetAxis("Horizontal");
+        V = Input.GetAxis("Vertical");
+        position.x = H;
+        position.y = V;
+        position.z = 0f;
+    }
+    private void FixedUpdate()
+    {
+        transform.Translate(position * speed * Time.fixedDeltaTime);
     }
 }
--- TOHOprojectBusan/Assets/Scripts/Ingame/PlayerAnim.cs
+++ TOHOprojectBusan/Assets/Scripts/Ingame/PlayerAnim.cs
@@ -2,14 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Reimu_anim : MonoBehaviour
+public class PlayerAnim : MonoBehaviour
 {
     private Animator myAnim;
     private bool isLeft;
     private bool isRight;
     private bool noMove = false;
-    private bool Stay = true;
-    private bool NoStay = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,18 +19,15 @@ public class Reimu_anim : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        myAnim.SetBool("Nomove", Stay);
         myAnim.SetBool("Left", noMove);
         myAnim.SetBool("Right", noMove);
 
-        if(Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
-            myAnim.SetBool("Nomove", NoStay);
             myAnim.SetBool("Left", isLeft);
         }
-        else if(Input.GetKey(KeyCode.RightArrow))
+        else if (Input.GetKey(KeyCode.RightArrow))
         {
-            myAnim.SetBool("Nomove", NoStay);
             myAnim.SetBool("Right", isRight);
         }
     }
