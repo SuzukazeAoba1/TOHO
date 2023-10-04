b771251742a5974d03207c782c1fd1e646bc685e
rasillrus <rasillrus@naver.com>
2023-10-04 18:14:41 +0900

    총알 destroy 수정

--- TOHOprojectBusan/Assets/Scripts/Ingame/Bullet.cs
+++ TOHOprojectBusan/Assets/Scripts/Ingame/Bullet.cs
@@ -6,11 +6,12 @@ public class Bullet : MonoBehaviour
 {
     public int ATK;
     public float Speed;
+    public float destroy_Time;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Destroy(gameObject, destroy_Time);
     }
 
     // Update is called once per frame
