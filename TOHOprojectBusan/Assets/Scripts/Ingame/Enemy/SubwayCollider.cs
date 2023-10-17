3b98bb6558030f910a1b040de872f37cf16adef2
JaeHO <noskang6699.naver.com>
2023-10-17 17:49:00 +0900

    지하철 작업중

---
+++ TOHOprojectBusan/Assets/Scripts/Ingame/Enemy/SubwayCollider.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SubwayCollider : MonoBehaviour
+{
+    private BoxCollider2D myCollider;
+    public float comeoffsetx = 49.704f;
+    public float comesizex = 100.41f;
+    public float outoffsetx = 99.183f;
+    public float outsizex = 1f;
+    public float duration = 0.6f;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        myCollider = GetComponent<BoxCollider2D>();
+    }
+    void Start()
+    {
+        StartCoroutine(Out());
+        Getsize(comeoffsetx, comesizex, duration);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Getsize(float targetOffset, float targetSize, float duration)
+    {
+        // offset 변화
+        LeanTween.value(gameObject, UpdateOffset, myCollider.offset.x, targetOffset, duration);
+
+        // size 변화
+        LeanTween.value(gameObject, UpdateSize, myCollider.size.x, targetSize, duration);
+    }
+
+    // LeanTween에서 사용할 함수로 offset을 업데이트
+    private void UpdateOffset(float value)
+    {
+        Vector2 currentOffset = myCollider.offset;
+        currentOffset.x = value;
+        myCollider.offset = currentOffset;
+    }
+
+    // LeanTween에서 사용할 함수로 size를 업데이트
+    private void UpdateSize(float value)
+    {
+        Vector2 currentSize = myCollider.size;
+        currentSize.x = value;
+        myCollider.size = currentSize;
+    }
+
+    IEnumerator Out()
+    {
+        yield return new WaitForSeconds(2f);
+        Getsize(outoffsetx, outsizex, duration);
+    }
+}
---
+++ TOHOprojectBusan/Assets/Scripts/Ingame/Enemy/SubwaySpawn.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SubwaySpawn : MonoBehaviour
+{
+    public int leftright = 0;
+    public float leftmin = 30;
+    public float rightmin = 150;
+    private void OnEnable()
+    {
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.rotation (new Vector3(0, 0, Random.Range(leftmin, leftmin + 60f)));
+    }
+}
