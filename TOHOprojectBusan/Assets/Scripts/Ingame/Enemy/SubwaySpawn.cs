019155b5cc19c8ef27da34a6acbebc5000faa54c
rogerthat1243 <lragusdn0@gmail.com>
2023-10-19 17:13:21 +0900

    적 패턴, 수량, 무기 공격력 등 수정

--- TOHOprojectBusan/Assets/Scripts/Ingame/Enemy/SubwaySpawn.cs
+++ TOHOprojectBusan/Assets/Scripts/Ingame/Enemy/SubwaySpawn.cs
@@ -7,6 +7,7 @@ public class SubwaySpawn : MonoBehaviour
     public int leftright = 0;
     public float rightmax = 30;
     public float leftmax = 210;
+    public float undermax = -60;
     public float destroy_time = 6.6f;
     private SpriteRenderer[] spriteRenderers;
     // Start is called before the first frame update
@@ -52,6 +53,13 @@ public class SubwaySpawn : MonoBehaviour
                 {
                     spriteRenderers[i].flipY = true;
                     spriteRenderers[i].flipX = true;
+                }
+                break;
+            case 4:
+                transform.rotation = Quaternion.Euler(0, 0, -90);
+                for (int i = 0; i < spriteRenderers.Length; i++)
+                {
+                    spriteRenderers[i].flipY = false;
                 }
                 break;
         }
